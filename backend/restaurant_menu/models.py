d65d9d99b6a4bf7808a6c32c5c60164052fc5402
PratikGarai <pratikgarai0208@gmail.com>
2021-01-03 15:51:06 +0530

    new model structure

--- backend/restaurant_menu/models.py
+++ backend/restaurant_menu/models.py
@@ -5,5 +5,10 @@ class Item(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     price = models.PositiveIntegerField()
-    image = models.URLField()
     rating = models.DecimalField(max_digits=3, decimal_places=2)
+
+
+class  ItemImage(models.Model):
+
+    url = models.URLField()
+    item = models.ForeignKey(Item, related_name="images", on_delete=models.CASCADE)
